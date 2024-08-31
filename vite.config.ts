e678bd4948595f03a757b4a27ed32b202437f52a
Thomás Furtado <thofurtado@gmail.com>
2024-08-30 21:20:18 -0300

    preparação para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/metrics",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
