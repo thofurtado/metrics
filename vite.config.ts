c9d2dd26aa04c9ad594b36838de3afe0aa9de7e4
Thomás Furtado <thofurtado@gmail.com>
2025-10-30 19:57:03 -0300

    teste

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: process.env.VITE_BASE_PATH || '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
