5caa5b20cd616ae8c26a78bad0cdaae247606935
Seu Nome <thofurtado@gmail.com>
2026-05-21 14:38:46 -0300

    Update auth.tsx

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import { MetricsIcon } from '../../components/MetricsIcon'
+import { Pyramid } from 'lucide-react'
 
 export function AuthLayout() {
   return (
