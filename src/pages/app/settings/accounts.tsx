3a2c91fc77e15d6bb817347b6493dadc908f04e7
Thomás Furtado <32690619+thofurtado@users.noreply.github.com>
2026-07-21 22:21:19 -0300

    fix: accounts.tsx jsx missing closing div

--- src/pages/app/settings/accounts.tsx
+++ src/pages/app/settings/accounts.tsx
@@ -249,6 +249,7 @@ export function Accounts() {
                     </DialogContent>
                 </Dialog>
             </div>
+            </div>
 
             <div className="grid gap-6 grid-cols-1 md:grid-cols-2 2xl:grid-cols-3">
                 {isLoading ? (
